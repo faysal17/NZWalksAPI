2865c79495f469ee4c931fed360ba4a338ae3aa4
Faysal Mahmud <82459799+faysal17@users.noreply.github.com>
2024-10-06 20:29:04 +0600

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        string[] studentsNames = new string[] { "faysal", "adnan", "rafat" };
+        [HttpGet]
+        public IActionResult getStudents()
+        {
+            return Ok(studentsNames);
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Difficulty.cs
@@ -0,0 +1,7 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Difficulty
+    {
+
+    }
+}
