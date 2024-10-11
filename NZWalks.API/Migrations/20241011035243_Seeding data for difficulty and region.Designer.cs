57f292647df8dffdc22e4b24621b960f217476be
Faysal Mahmud <82459799+faysal17@users.noreply.github.com>
2024-10-11 09:53:14 +0600

    Seed the difficulty and region data.

--- NZWalks.API/Migrations/20241011035243_Seeding data for difficulty and region.Designer.cs
+++ NZWalks.API/Migrations/20241011035243_Seeding data for difficulty and region.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NZWalks.API.Data;
 
@@ -11,9 +12,11 @@ using NZWalks.API.Data;
 namespace NZWalks.API.Migrations
 {
     [DbContext(typeof(NZWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241011035243_Seeding data for difficulty and region")]
+    partial class Seedingdatafordifficultyandregion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
